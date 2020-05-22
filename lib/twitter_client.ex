f3ded93d34311fc17ea6f53735327c3f8d4305c9
Allef Gomes <allefgalmeida@gmail.com>
2020-05-22 17:43:55 -0300

    Initialize project

---
+++ lib/twitter_client.ex
@@ -0,0 +1,18 @@
+defmodule TwitterClient do
+  @moduledoc """
+  Documentation for `TwitterClient`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TwitterClient.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
